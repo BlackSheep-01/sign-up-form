39f621b3623b641e8f5f9c1f6d36470d4bbfc0ab
Nilkanta Roy <roynilkanta2000@gmail.com>
2023-11-05 19:36:52 +0530

    Add user-interface of sign-up form

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# sign-up-form
+# Summary :
+This is a demo Sign-up Form build as a part of the Odin Project curriculum.Certain form validations are associated with each form field. It doesn't have a backend so it can't store any data entered by the user.
+
+# Live Preview : [Sign-up Form](https://bluelordd.github.io/sign-up-form/)
+
+# Languages and technologies used :
+HTML/CSS/JavaScript
+VS Code
+Linux terminal
+Git and Github
+Chrome developer tools
---
+++ script.js
@@ -0,0 +1,21 @@
+
+  
+function validateForm() {
+    checkPasswordMatch(); // Run the check before form submission
+    const form = document.getElementById("registration-form");
+    if (form.checkValidity()) 
+        return true; // Form submission proceeds when the form is valid
+    else
+        return false; // Prevent form submission when there are validation errors
+}
+  
+
+function checkPasswordMatch() {
+    const password = document.getElementById("user-password").value;
+    const confirmPassword = document.getElementById("user-password-confirm").value;
+    
+    if (password === confirmPassword) 
+        document.getElementById("user-password").setCustomValidity(""); // Clear custom message
+    else
+        document.getElementById("user-password-confirm").setCustomValidity("Passwords do not match.");
+}
